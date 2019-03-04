dec8a9f5014124b62e289f3a85877061765212f0
Vanessa Henderson <bookvanny@gmail.com>
2019-03-04 09:23:59 -0500

    methods done yay

--- data_types.py
+++ data_types.py
@@ -5,12 +5,18 @@
 #     - "windows_count" -> data_type: int, value: 8
 #     - "glare" -> data_type: boolean, value: True
 
+dictionary_classroom = {
+    "chairs": 35,
+    "name": 'CR3',
+    "windows_count": 8,
+    "glare": True
+}
 
 # #2: Create a list called "num_list" withs values 1 through 4 in it:
 
-
+num_list = [1, 2, 3, 4]
 # #3: Create a list called "teacher_list" with the strings "zakk", "don",
 # "jimmy", and "hector" in it:
 
-
+teacher_list = ["zakk", "don", "jimmy", "hector"]
 # Commit when you finish working on these questions!
